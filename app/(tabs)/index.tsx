9cef288e30182c739773a467db3010d84a427cac
jaypms <beatsmarket.best@gmail.com>
2025-07-27 11:15:40 +0200

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-import { View } from "react-native";
-import BobMobile from "../../../src/components/BobMobile";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1 }}>
-      <BobMobile />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 24, color: "purple" }}>🎉 TEST BOB OK 🎉</Text>
     </View>
   );
 }
